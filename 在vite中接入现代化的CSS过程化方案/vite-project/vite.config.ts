18cb0cfa0df459998146a1cc4ade576d8ae656e4
nnjjjcc <3291428201@qq.com>
2023-06-20 23:32:36 +0800

    第二部分

--- 在vite中接入现代化的CSS过程化方案/vite-project/vite.config.ts
+++ 在vite中接入现代化的CSS过程化方案/vite-project/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import autoprefixer from 'autoprefixer';
+//自动添加浏览器前缀。
 import { normalizePath } from 'vite';
+//调用normalizePath函数并传递路径作为参数：
 import path from 'path';
 // 全局 scss 文件的路径
 // 用 normalizePath 解决 window 下的路径问题
@@ -14,11 +17,26 @@ export default defineConfig({
   plugins: [react()],
   // css 相关的配置
   css: {
+    //指定预处理器的相关配置
     preprocessorOptions: {
       scss: {
         // additionalData 的内容会在每个 scss 文件的开头自动注入
         additionalData: `@import "${variablePath}";`
       }
+    },
+    modules: {
+      //通过generateScopedName 属性对生成的类名进行自定义
+      //name表示当前文件名，local表示类名
+      generateScopedName: "[name]__[local]__[hash:base64:5]"
+      //[hash:base64:5] 表示对类名进行哈希编码后的 5 位长度的 base64 字符串。这种命名方式确保了在不同文件和组件之间生成的类名是唯一的，避免了类名冲突的问题。
+    },
+    postcss: {
+      plugins: [
+        autoprefixer({
+          overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11']
+          // 兼容 Chrome 版本大于 40、Firefox 版本大于 31 和 IE 11。
+        })
+      ]
     }
   }
 
